dd9015dedc38059b0e87d4e3233bb89820c1084b
Chuma Takahiro <chuma@unicorn.limited>
2023-10-24 12:18:13 +0900

    Fix: add args

--- pg-pool/tests/integration/prepare.rs
+++ pg-pool/tests/integration/prepare.rs
@@ -3,7 +3,7 @@ use pg_pool::{pg, pgr, Type};
 #[tokio::test]
 async fn invalid_prepare_statement() {
     let stmt = pg::prepare_typed_cached("SELECT 1 + $1", &[Type::INT8]).await.unwrap();
-    let row = pg::query_one(&stmt, &[]).await.unwrap();
+    let row = pg::query_one(&stmt, &[&8i64]).await.unwrap();
 
     pg::execute("DEALLOCATE ALL", &[]).await.unwrap();
     let result = pg::query_one(&stmt, &[&9i64]).await;
